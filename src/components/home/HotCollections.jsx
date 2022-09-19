a738ccccad9efe04b389e4e93d930f8aba55757a
Brennan Lonidier <brennanlonidier@gmail.com>
2022-09-18 20:21:42 -0500

    Animation and Bug Fixes

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -111,33 +111,33 @@ const HotCollections = () => {
             <div className="navigation-wrapper">
               <div ref={sliderRef} className="keen-slider">
                 {hotCollections.map((item, index) => (
-                  <div className="keen-slider__slide" key={index}>
-                    <div className="nft_coll">
-                      <div className="nft_wrap">
-                        <Link to={`/item-details/${item.nftId}`}>
-                          <img
-                            src={item.nftImage}
-                            className="lazy img-fluid"
-                            alt=""
-                          />
-                        </Link>
-                      </div>
-                      <div className="nft_coll_pp">
-                        <Link to={`/author/${item.authorId}`}>
-                          <img
-                            className="lazy pp-coll"
-                            src={item.authorImage}
-                            alt=""
-                          />
-                        </Link>
-                        <i className="fa fa-check"></i>
-                      </div>
-                      <div className="nft_coll_info">
-                        <Link to={`/item-details/${item.nftId}`}>
-                          <h4>{item.title}</h4>
-                        </Link>
-                        <span>ERC-{item.code}</span>
-                      </div>
+                 <div className="keen-slider__slide" key={index}>
+                 <div className="nft_coll">
+                   <div className="nft_wrap">
+                     <Link to={`/item-details/${item.nftId}`}>
+                       <img
+                         src={item.nftImage}
+                         className="lazy img-fluid"
+                         alt=""
+                       />
+                     </Link>
+                   </div>
+                   <div className="nft_coll_pp">
+                     <Link to={`/author/${item.authorId}`}>
+                       <img
+                         className="lazy pp-coll"
+                         src={item.authorImage}
+                         alt=""
+                       />
+                     </Link>
+                     <i className="fa fa-check"></i>
+                   </div>
+                   <div className="nft_coll_info">
+                     <Link to={`/item-details/${item.nftId}`}>
+                       <h4>{item.title}</h4>
+                     </Link>
+                     <span>ERC-{item.code}</span>
+                   </div>
                     </div>
                   </div>
                 ))}
